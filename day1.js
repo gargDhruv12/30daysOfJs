6b1a1a140a6379a250ae1de86d4f4f3759f3a776
“gargdhruv12” <dhruvgarg729729@gmail.com>
2024-07-20 00:15:12 +0530

    Added day1.js

---
+++ day1.js
@@ -0,0 +1,52 @@
+// Activity 1: Variable Declaration
+
+// Task 1: Declare a variable using `var`, assign it a number, and log the value to the console.
+var num = 2;
+console.log(num); //output : 2
+
+// Task 2: Declare a variable using `let`, assign it a string, and log the value to the console.
+let str = "Dhruv";
+console.log(str); //output : Dhruv
+
+// Activity 2: Constant Declaration
+
+// Task 3: Declare a variable using `const`, assign it a boolean value, and log the value to the console.
+const check = true;
+console.log(check); //output : true
+
+// Activity 3: Data Types
+
+// Task 4: Create variables of different data types and log each variable's type using the `typeof` operator.
+let num1 = 28;
+var bool = false;
+const arr = [1,2,7,9];
+let string = "NIT kkr";
+let xyz;
+let obj = {name :"Dhruv",year : 2};
+
+console.log(typeof num1); //output : number
+console.log(typeof bool); //output : boolean
+console.log(typeof arr); //output : object
+console.log(typeof string); //output : string
+console.log(typeof xyz); //output : undefined
+console.log(typeof obj); //output : object
+
+// Activity 4: Reassigning Variables
+
+// Task 5: Declare a variable using `let`, assign it an initial value, reassign a new value, and log both values to the console.
+let value = "Old value";
+console.log(value); //output : Old value
+value = "new value";
+console.log(value); //output : new value
+
+// Activity 5: Understanding `const`
+
+// Task 6: Try reassigning a variable declared with `const` and observe the error.
+const check_num = 23; //output : 23
+check_num = 28; // Error: Assignment to constant variable
+
+
+
+
+
+
